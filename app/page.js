ee611343792698081071d16367aa9fdb333b6125
Golliard <golliard.info@gmail.com>
2024-11-13 21:40:40 +0100

    desactive Eslink le 13.11 à 21:40

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-      <div className="flex items-center justify-center h-screen bg-white">
-          <h1 className="text-green-500 text-3xl font-bold">Coordonné avec le nom et l&apos;email</h1>
-      </div>
+    <div className="flex items-center justify-center h-screen bg-white">
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <h1 className="text-green-500 text-3xl font-bold">Coordonné avec le nom et l'email</h1>
+    </div>
   );
 }
