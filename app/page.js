91403be1e00dac92bb9729d90a66f67a5ae0c955
Golliard <golliard.info@gmail.com>
2024-11-13 21:07:34 +0100

    mise a jour le 13.11 a 21:07

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div className="flex items-center justify-center h-screen bg-white">
-          <h1 className="text-green-500 text-3xl font-bold">Coordonné avec ton nom et prénom</h1>
+          <h1 className="text-green-500 text-3xl font-bold">Coordonné avec le nom et l'email</h1>
       </div>
   );
 }
