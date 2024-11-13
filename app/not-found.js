2583a69ba70238399925a4a5d2a547369597666f
Golliard <golliard.info@gmail.com>
2024-11-13 15:16:02 +0100

    correction apostrophe

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,9 @@ export default function NotFound() {
     return (
         <div className="text-center p-8">
             <h1 className="text-3xl font-bold text-red-500">Page introuvable</h1>
-            <p className="text-gray-700">Le contenu que vous cherchez n'existe pas.</p>
+            <p className="text-gray-700">
+                Le contenu que vous cherchez n&#39;existe pas.
+            </p>
         </div>
     );
 }
