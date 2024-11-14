558057eda46fac8be7b542dac1c0dc106765d6bd
Golliard <golliard.info@gmail.com>
2024-11-14 15:08:00 +0100

    mise a jour le 14.11 à 15:08

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
       {/* eslint-disable-next-line react/no-unescaped-entities */}
-      <h1 className="text-green-500 text-3xl font-bold">Correction le 14.11.2024 à 15:58</h1>
+      <h1 className="text-green-500 text-3xl font-bold">Correction le 14.11.2024 à 15:08</h1>
     </div>
   );
 }
